0c957161cb78615df1271e2cecfc9dc45f50905d
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2024-01-03 22:47:21 +0530

    Add files via upload

---
+++ testclass.java
@@ -0,0 +1,27 @@
+//using constructors to get the values
+public class testclass{
+    private float len;
+    private float width;
+    private float height;
+    testclass(float l,float w,float h){
+len=l;
+width=w;
+height= h;
+    }
+    public float getl(){
+        return len;
+    }
+    public float getw(){
+        return width; 
+      }
+    public float geth(){
+        return height;
+    }
+
+// public class testclass {
+   public static void main(String[ ] args){
+    testclass testclass1 = new testclass(5,7,9); 
+System.out.printf("The height of testclass is :%.1f , width :%.1f, height:%.1f",testclass1.getl(),testclass1.getw() ,testclass1.geth());
+   }
+}
+
