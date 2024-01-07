b2060c647bcdbd09c8e0df7a05994a956de12cd5
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2024-01-07 17:53:58 +0530

    Add files via upload

---
+++ math.java
@@ -0,0 +1,15 @@
+public class math {
+    public static void main(String[] args) {
+      System.out.println(Math.E);
+      System.out.println(Math.PI); //double
+      System.out.println(Math.abs(-12.1)); // give sthe ab value
+      System.out.println(Math.pow(2,6));
+      System.out.println(Math.ceil(56.02)); //give sthe nearrest larger value
+      System.out.println(Math.floor(.365));//give sthe nearrest smaller value
+      System.out.println(Math.round(5.1547896));
+      System.out.println(Math.cbrt(27));
+      System.out.println(Math.sqrt(16));
+      System.out.println(Math.copySign(-4,2));//returns value of first and sign of second
+      System.out.println(Math.random());
+    }
+}
