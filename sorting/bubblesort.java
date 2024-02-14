7e1aefbc1eb8763d65a5ce048dda24829da6fc61
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2024-02-14 23:23:15 +0530

    Add files via upload

---
+++ sorting/bubblesort.java
@@ -0,0 +1,19 @@
+package sorting;
+
+public class BubbleSort {
+    public void bubblesort(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+               if(arr[j]>arr[j+1]){
+                int temp  =arr[j+1];
+                arr[j+1] =arr[j];
+                arr[j] = temp;
+               }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
